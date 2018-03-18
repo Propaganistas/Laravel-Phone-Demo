ffea734926e9d92301928626db8cd6e49738c2f6
Propaganistas <propaganistas@gmail.com>
2018-03-18 10:52:18 +0100

    Show version number

--- config/app.php
+++ config/app.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Laravel Phone demo'),
+    'package_version' => env('PACKAGE_VERSION', '???'),
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/html.blade.php
+++ resources/views/html.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.name') }} {{ config('app.package_version') }}</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 
@@ -20,12 +20,12 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <span class="navbar-brand" href="javascript:void(0);">{{ config('app.name') }}</span>
+                <span class="navbar-brand" href="javascript:void(0);">{{ config('app.name') }} ({{ config('app.package_version') }})</span>
             </div>
             <div id="navbar" class="collapse navbar-collapse">
                 <form class="navbar-form navbar-right">
                     <div class="form-group">
-                        <label for="count">Aantal velden:</label>
+                        <label for="count"># Fields:</label>
                         <input type="text" id="count" name="count" class="form-control navbar-count" value="{{ request('count', 1) }}">
                     </div>
                 </form>
