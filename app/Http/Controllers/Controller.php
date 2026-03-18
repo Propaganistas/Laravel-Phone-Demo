0751f836b6e71c28ffc97538038e067ba5f25610
Propaganistas <Propaganistas@users.noreply.github.com>
2026-03-18 08:15:44 +0100

    update to L13

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Arr;
 
 class Controller extends BaseController
 {
